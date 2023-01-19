e981f94e5971a94bef086324e7ce8f4b9d85bb8d
Alexander Repeta <alexander.repeta@gmail.com>
2023-01-19 18:21:59 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,12 +6,10 @@ import { GlobalStyle } from 'components/GlobalStyle';
 import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
   <BrowserRouter>
     <ThemeProvider theme={theme}>
       <App />
       <GlobalStyle />
     </ThemeProvider>
   </BrowserRouter>
-  // </React.StrictMode>
 );
